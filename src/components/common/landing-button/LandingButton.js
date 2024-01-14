48d02ef2bec90a435d649c7829b412b53549db41
Bryan E Amaro <bryaneamaro@gmail.com>
2024-01-14 02:00:49 -0500

    1st_buttons_and_login_pages

--- src/components/common/landing-button/LandingButton.js
+++ src/components/common/landing-button/LandingButton.js
@@ -1,19 +1,29 @@
 import "../login/Login.css"
+import {Link} from "react-router-dom"
 
 const LandingButton = ({isStudent, setStudent}) => {
 
 
   return (
     <div>
-        <button 
+        <button
           className= {`landing-btn student ${isStudent ? "btn-active" : ""}`} 
-          onClick={() => { setStudent(true); console.log("student")}}>
-            Student
+          onClick={() => { setStudent(true); console.log("student btn clicked")
+          }}>
+
+            <Link to="/login" style={{ textDecoration: 'none' }} >
+              Student
+            </Link>
+
         </button>
-        <button 
+        <button
           className= {`landing-btn business ${!isStudent ? "btn-active" : ""}`} 
-          onClick={() => { setStudent(false);console.log("business")}}>
-            Business
+          onClick={() => { setStudent(false);console.log("business btn clicked")}}>
+
+            <Link to="/login" style={{ textDecoration: 'none' }} >
+              Business
+            </Link>
+
         </button>
     </div>
   )
