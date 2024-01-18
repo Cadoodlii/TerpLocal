f344dab561492ce31368b7a644e6c6a66a16f364
Cadoodlii <catherineli321@gmail.com>
2024-01-17 21:01:43 -0500

    Changed the projects
    Added an image and url field to the projects. Also changed the projects to include actual DMV businesses.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ import {
 import { Routes, Route } from "react-router-dom";
 import LandingButton from "./components/common/landing-button/LandingButton";
 
+import LittleBoyBakeryImage from "./images/LittleBoyBakery.png";
+import ValenciasProduceImage from "./images/ValenciasProduce.png";
+import RandaliaBeehivesImage from "./images/RandaliaBeehives.png";
+import WoodsFlowersAndGiftsImage from "./images/WoodsFlowersAndGifts.png";
+import FairDaysPlay from "./images/FairDaysPlay.png";
+import FiasFabulousFinds from "./images/FiasFabulousFinds.png";
+
 function App() {
   const [isStudent, setStudent] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -37,26 +44,62 @@ function App() {
     {
       id: 1,
       title: "Build a Website",
-      company: "Three Little Birds Sewing Co",
+      company: "Valencia's Produce",
       major: "computer science",
       description:
-        "Help us build the frontend of our website! Looking for computer science majors with at least 1 year of web development experience",
+        "Help us build our website! Looking for computer science majors with at least 1 year of web development experience",
+      image: ValenciasProduceImage,
+      url: "https://www.instagram.com/valenciasproduce/",
     },
     {
       id: 2,
-      title: "Design a Logo",
-      company: "Board and Brew",
+      title: "Design a New Logo",
+      company: "Wood's Flowers And Gifts",
       major: "art",
       description:
         "Help us design a logo for our business! We are looking for an updated logo that incorporates both the drinks and games aspect of our restaurant. Looking for students skilled in art/graphic design",
+      image: WoodsFlowersAndGiftsImage,
+      url: "https://www.woodsflowersandgifts.com/",
     },
     {
       id: 3,
       title: "Make a Marketing Campaign",
-      company: "Vigilante Coffee Co",
+      company: "Little Boy Bakery",
       major: "business",
       description:
         "Help us grow our social media account by creating a marketing campaign plan. Looking for students who have experience in marketing",
+      image: LittleBoyBakeryImage,
+      url: "https://littleboybakery.com/",
+    },
+    {
+      id: 4,
+      title: "Design a Logo",
+      company: "Fair Day's Play",
+      major: "art",
+      description:
+        "Help us design a logo for our business! We're a small business dedicated providing kids with fun and unique toys.",
+      image: FairDaysPlay,
+      url: "https://fairdaysplay.com/",
+    },
+    {
+      id: 5,
+      title: "Manage Our Socials",
+      company: "Randalia Beehives",
+      major: "business",
+      description:
+        "Help us grow our social media account by creating a marketing campaign plan. Looking for students who have experience in marketing",
+      image: RandaliaBeehivesImage,
+      url: "https://randaliafarm.com/",
+    },
+    {
+      id: 6,
+      title: "Design a Logo",
+      company: "Fia's Fabulous Finds",
+      major: "art",
+      description:
+        "Help us design a logo for our business! We're a small business dedicated to upscaling garments that have previously been donated.",
+      image: FiasFabulousFinds,
+      url: "https://www.facebook.com/Fiasfabfinds/",
     },
   ]);
 
